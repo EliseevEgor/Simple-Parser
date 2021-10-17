7168d3a345c7ed4b05f66f2e2316925495b75bb5
EliseevEgor <gorgpupkin@gmail.com>
2021-10-17 14:40:37 +0300

    Update start command

--- telegram_bot/constants.py
+++ telegram_bot/constants.py
@@ -1,6 +1,12 @@
 from server.utils.constants import URL
 
 BOT_TOKEN = "1958658356:AAGlYhFAPZg-1B_qC9tfB0vPFuK3G_unsOU"
-HELLO_MESSAGE = f"Hello!\nInformation about which company do you want to know from the page {URL}?"
+HELLO_MESSAGE = f"Привет!\n" \
+                f"Вы можете получить информацтю про интересующую Вас компанию\n" \
+                f"Для этого напишите ее код(например PD)\n" \
+                f"Все компании можно посмотреть тут {URL}\n" \
+                f"Можно добавить компанию в избранное: Добавь компанию \"код\"\n" \
+                f"Удалить компанию из избранного: Удали компанию \"код\"\n" \
+                f"Показать компании из избранного: Покажи мои компании"
 EXCEPTION_CANT_FIND = "Я не смог найти такую компанию"
 EXCEPTION_ALL_ELSE = "Ой-ёй, что-то пошло не так"
